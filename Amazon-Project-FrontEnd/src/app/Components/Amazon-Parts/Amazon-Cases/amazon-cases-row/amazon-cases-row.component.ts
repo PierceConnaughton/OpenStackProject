21620246bf1e0811d5c87feabf6622dcbdabfb90
Ti43652 <47326833+Ti43652@users.noreply.github.com>
2021-04-08 05:59:11 +0100

    Added Star Rating to Amazon Lists

--- Amazon-Project-FrontEnd/src/app/Components/Amazon-Parts/Amazon-Cases/amazon-cases-row/amazon-cases-row.component.ts
+++ Amazon-Project-FrontEnd/src/app/Components/Amazon-Parts/Amazon-Cases/amazon-cases-row/amazon-cases-row.component.ts
@@ -9,6 +9,14 @@ import { AmazonCasesResponse } from '../../../../model/AmazonCasesResponse';
 export class AmazonCasesRowComponent implements OnInit {
 
   @Input() case: AmazonCasesResponse;
+
+  imageOneStar: string = "https://image.shutterstock.com/image-vector/one-stars-rating-button-experience-260nw-1705308238.jpg";
+  imageTwoStar: string = "https://image.shutterstock.com/image-vector/two-stars-icon-vector-260nw-1316819486.jpg";
+  imageThreeStar: string = "https://image.shutterstock.com/image-vector/three-stars-icon-vector-260nw-1316819480.jpg";
+  imageHalfFourStar:string = "https://newnoisemagazine.com/wp-content/uploads/2019/10/4-half-stars.jpg";
+  imageFourStar: string = "https://library.kissclipart.com/20180904/xwe/kissclipart-3-out-of-5-stars-clipart-far-cry-5-review-mafia-ii-29bfffd9031764c0.jpg";
+  imageFiveStar: string = "https://drammanda.files.wordpress.com/2012/07/5-red-stars.jpg";
+
   constructor() { }
 
   ngOnInit(): void {
