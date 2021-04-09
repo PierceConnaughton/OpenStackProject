c9c211603de214a2dce50b3765b30d2ca03d4895
Ti43652 <47326833+Ti43652@users.noreply.github.com>
2021-04-09 18:37:56 +0100

    Added nginx.conf

--- Amazon-Project-FrontEnd/src/environments/environment.prod.ts
+++ Amazon-Project-FrontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000',
-  facebookAppId: '413281543384405'
+  apiUrl: 'https://localhost:8080',
+  facebookAppId: '413281543384405',
+  firebase : {
+    apiKey: "AIzaSyBuL4HEzb1Ka-XwaDD2aOnLCKi1bQKLCMk",
+    authDomain: "open-stack-project-bee7f.firebaseapp.com",
+    databaseURL: "https://open-stack-project-bee7f-default-rtdb.firebaseio.com",
+    projectId: "open-stack-project-bee7f",
+    storageBucket: "open-stack-project-bee7f.appspot.com",
+    messagingSenderId: "439359786750",
+    appId: "1:439359786750:web:d8ce83e63ef5f3e9d15faf"
+  }
 };
